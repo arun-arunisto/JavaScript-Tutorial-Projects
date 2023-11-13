e59575b48eb0f69f36618e7a609cdaab356de90f
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2023-11-13 20:12:30 +0530

    Add files via upload

---
+++ TODOAPP/script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!!"); 
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // this code is the 'x' in the list
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// storing/saving data into local storage browser
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// showing data saved in localStorage
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask(); 
